e795466c83f06d3a645e30f12f0169ad8f9f3bf7
Alice Stephanie Soares RIbeiro <alicessr12@gmail.com>
2023-07-16 21:39:01 -0300

    aula 41

--- EstruturasCondicionais/Program.cs
+++ EstruturasCondicionais/Program.cs
@@ -4,15 +4,23 @@
     {
         static void Main(string[] args)
         {
-            int valor = 5;
+            int valor = 25;
 
-            if (valor > 5) 
+            if (valor < 5) 
             {
                 Console.WriteLine("Condição verdadeira");
             }
+            else if (valor >= 5 && valor <10)
+            {
+                Console.WriteLine("Condição alternativa 1");
+            }
+            else if (valor >= 10 && valor < 20)
+            {
+                Console.WriteLine("Condição alternativa 2");
+            }
             else
             {
-                Console.WriteLine("Condição retornou falso");
+                Console.WriteLine("Condição alternativa final");
             }
 
             Console.ReadKey();
