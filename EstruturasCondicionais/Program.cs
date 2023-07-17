9c21891deec257c525814277d761a8c8f8940251
Alice Stephanie Soares RIbeiro <alicessr12@gmail.com>
2023-07-16 21:29:32 -0300

    aula 40

---
+++ EstruturasCondicionais/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace EstruturasCondicionais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor = 5;
+
+            if (valor > 5) 
+            {
+                Console.WriteLine("Condição verdadeira");
+            }
+            else
+            {
+                Console.WriteLine("Condição retornou falso");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
