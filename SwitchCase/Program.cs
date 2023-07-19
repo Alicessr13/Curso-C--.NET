91bfc43ced47492c1f36ba65657483f0a6e0453b
Alice Stephanie Soares RIbeiro <alicessr12@gmail.com>
2023-07-19 19:59:54 -0300

    aula 45

---
+++ SwitchCase/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace SwitchCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int escolha = 1;
+
+            switch (escolha)
+            {
+                case 1:
+                    Console.WriteLine("Opção 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Opção 1");
+                    break;
+                case 3:
+                    Console.WriteLine("Opção 1");
+                    break;
+
+                default:
+                    Console.WriteLine("Opção default");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
