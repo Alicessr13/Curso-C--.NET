2694d86c33ce5af93517ae49bf42146fad285de3
Alice Stephanie Soares RIbeiro <alicessr12@gmail.com>
2023-07-20 20:23:13 -0300

    aula 48

---
+++ EstruturasRepetição/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace EstruturasRepetição
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor = 0;
+
+            while (valor<10)
+            {
+                Console.WriteLine("O valor é "+valor);
+                valor++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
