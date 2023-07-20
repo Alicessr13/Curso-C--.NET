bcd4d1dcfb3de66536dc3f8fa4c57f7a373852d0
Alice Stephanie Soares RIbeiro <alicessr12@gmail.com>
2023-07-20 20:28:06 -0300

    aula 50

--- EstruturasRepetição/Program.cs
+++ EstruturasRepetição/Program.cs
@@ -4,14 +4,28 @@
     {
         static void Main(string[] args)
         {
+            #region while
             int valor = 0;
 
             while (valor<10)
             {
-                Console.WriteLine("O valor é "+valor);
+                //Console.WriteLine("O valor é "+valor);
                 valor++;
             }
+            #endregion
 
+            #region do while
+
+            int valor2 = 15;
+
+            do
+            {
+                Console.WriteLine("O valor é " + valor2);
+                valor2++;
+
+            } while (valor2<10);
+
+            #endregion
             Console.ReadKey();
         }
     }
