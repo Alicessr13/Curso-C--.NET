02ba7c02b292b1d8f67cc1a8b4cf5b4f89009d42
Alice Stephanie Soares RIbeiro <alicessr12@gmail.com>
2023-07-20 21:19:52 -0300

    aula 52

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Drawing;
+
+namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0, controle = 1, result=0;
+
+
+            Console.WriteLine("Quantos valores: ");
+            int valores = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Sequencia de Fibonacci com " + valores + " valores");
+
+            for(int i = 0; i < valores; i++)
+            {
+                if(i <valores - 1)
+                {
+                    Console.Write(num + ", ");
+                }
+                else
+                {
+                    Console.Write(num + " ");
+                }
+                
+
+                result = num + controle;
+                num = controle;
+                controle = result;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
