1c71b5f4701e3124ef2ae4afc6fc97213003f02e
Alice Stephanie Soares RIbeiro <alicessr12@gmail.com>
2023-07-26 23:14:08 -0300

    aula 54_2

---
+++ POO/ClassesObjetos/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ClassesObjetos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MinhaClasse mClasse = new MinhaClasse(); //criação de objeto 
+            MinhaClasse m2 = null; //objeto não inicializado
+
+            OutraClasse Outra = new OutraClasse();
+            OutraClasse Outra2 = Outra; //atribui a referencia do objeto
+        }
+    }
+
+    class MinhaClasse 
+    {
+
+    }
+
+}
