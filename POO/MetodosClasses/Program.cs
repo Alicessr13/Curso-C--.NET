2a77e1eb8069efe6540b7144d3b0fb47c5788a48
Alice Stephanie Soares RIbeiro <alicessr12@gmail.com>
2023-07-27 21:36:16 -0300

    aula 57

---
+++ POO/MetodosClasses/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace MetodosClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Metodos m = new Metodos();
+            m.Comprimentar();
+            m.Somar(23,42);
+            m.Apresentar("Alice", 20);
+
+            Console.ReadKey();
+
+        }
+    }
+}
