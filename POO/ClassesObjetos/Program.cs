72069e6c2d5714bbc7400fc8aacbbb1bb7e7feeb
Alice Stephanie Soares RIbeiro <alicessr12@gmail.com>
2023-07-27 21:09:40 -0300

    aula 55

--- POO/ClassesObjetos/Program.cs
+++ POO/ClassesObjetos/Program.cs
@@ -4,11 +4,41 @@
     {
         static void Main(string[] args)
         {
-            MinhaClasse mClasse = new MinhaClasse(); //criação de objeto 
-            MinhaClasse m2 = null; //objeto não inicializado
+            #region Classes e objetos
+            //MinhaClasse mClasse = new MinhaClasse(); //criação de objeto 
+            //MinhaClasse m2 = null; //objeto não inicializado
 
-            OutraClasse Outra = new OutraClasse();
-            OutraClasse Outra2 = Outra; //atribui a referencia do objeto
+            //OutraClasse Outra = new OutraClasse();
+            //OutraClasse Outra2 = Outra; //atribui a referencia do objeto não o valor 
+            #endregion
+
+            Pessoa p1 = new Pessoa();
+            p1.nome = "Alice";
+            p1.sobrenome = "Stephanie";
+            p1.anoNascimento = 2003;
+
+            Pessoa p2 = new Pessoa()
+            {
+                nome = "Artur", 
+                sobrenome = "Ribeiro",
+                anoNascimento = 1985
+            };
+
+            p1.sobrenome = "Soares";
+
+            Console.WriteLine("Pessoa 1: " + p1.nome);
+            Console.WriteLine("Pessoa 1: " + p1.sobrenome);
+            Console.WriteLine("Pessoa 1: " + p1.anoNascimento);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Pessoa 2: " + p2.nome);
+            Console.WriteLine("Pessoa 2: " + p2.sobrenome);
+            Console.WriteLine("Pessoa 2: " + p2.anoNascimento);
+
+            Console.ReadKey();
+
+
         }
     }
 
