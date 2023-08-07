076450fbd63a57ea1f77dc1f1862cb497ccc92ab
Alice Stephanie Soares RIbeiro <alicessr12@gmail.com>
2023-08-07 17:24:04 -0300

    Aula 61

---
+++ POO/TermoThis/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace TermoThis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Acessar a = new Acessar();
+
+            if (a.Login("abc123"))
+            {
+                Console.WriteLine("Seja bem vindo! ");
+            }
+            else
+            {
+                Console.WriteLine("Acesso negado! ");
+            }
+        }
+    }
+}
