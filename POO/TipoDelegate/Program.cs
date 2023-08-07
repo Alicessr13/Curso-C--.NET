c1d71299f48947cf077bfcd0f738b86f27d71d77
Alice Stephanie Soares RIbeiro <alicessr12@gmail.com>
2023-08-07 18:11:08 -0300

    aula 62

---
+++ POO/TipoDelegate/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace TipoDelegate
+{
+    internal class Program
+    {
+        delegate void Operacao(int num1, int num2);
+        static void Main(string[] args)
+        {
+            //delegate e um tipo personalizado
+            Matematica m = new Matematica();
+
+            Operacao conta = null;
+
+            conta += m.Somar; // o objeto delegate vai se comportar como sendo o metodo somar
+            //vai retornar tudo que o metodo faz
+
+            conta += m.Subtração;
+            conta += m.Multiplicar;
+            conta += m.Dividir;
+            conta += m.Multiplicar;
+
+            conta(10, 2);
+
+            Console.WriteLine();
+
+            conta -= m.Dividir;
+            conta -= m.Subtração;
+
+            conta(15, 3);
+
+            Console.ReadKey();
+        }
+    }
+}
