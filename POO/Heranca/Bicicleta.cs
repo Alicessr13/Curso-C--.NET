f5997ebc05d542c0fcde81b1f43c9d9a718c9ad2
Alice Stephanie Soares RIbeiro <alicessr12@gmail.com>
2023-08-21 19:35:50 -0300

    Aula 70
    Herança

---
+++ POO/Heranca/Bicicleta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heranca
+{
+    internal class Bicicleta : Veiculo // : significa herança em c#
+    {
+        public void Pedalar()
+        {
+
+        }
+    }
+}
---
+++ POO/Heranca/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Heranca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //uma classe herda atributos metodos e comportamentos de outra classe
+            Carro c = new Carro();
+            Bicicleta b = new Bicicleta();
+
+        }
+    }
+}
